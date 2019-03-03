1b8dee1e36080758f1bec3cc615abcb934efcaf3
Wajahat-Syed <39873304+Wajahat-Syed@users.noreply.github.com>
2019-03-03 22:28:38 +0530

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,41 +1,58 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
 <!-- footer -->
 <section class="footer">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-3">
-				<div class="about">
-					<a class="" href="index.html"> <img class="img-fluid" src="images/sufiana-white.png" alt=""></a>
-					<h6>It is a long established fact that a reader will be distracted by the readable content of a h6age when looking at its layout.cover many web sites still in their forn comh6nay alh6habet infancy.</h6>
+	<div class="container-fluid">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3">
+					<div class="about">
+						<a class="" href="index.html"> <img class="img-fluid" src="images/sufiana-white.png" alt=""></a>
+						<h6>It is a long established fact that a reader will be distracted by the readable content of a h6age when looking at its layout.cover many web sites still in their forn comh6nay alh6habet infancy.</h6>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="pages ml-4">
+						<h3 class="mt-4">Pages</h3>
+						<div class="underline"></div>
+						<ul class="list-unstyled mt-4">
+							<li><a href="#">home</a> </li>
+							<li><a href="#">current opening</a> </li>
+							<li><a href="#">Walk in</a> </li>
+							<li><a href="#">faq</a> </li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="contact">
+						<h3 class="mt-4">Contact</h3>
+						<div class="underline"></div>
+						<p class="mt-4">Address: 3rd Floor, Building No. 261, Lane No 5, Westend Marg, Said-ul-Ajaib, Saket</p>
+						<p>Phone : 12554854555</p>
+						<p><i class="fa fa-phone" aria-hidden="true"></i> +91-9891408339/9540078339</p>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<iframe class="facebookfeeds" src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2FMercifulServantHD&width=250&colorscheme=light&show_faces=true&border_color&stream=true&header=true&height=400"
+					scrolling="yes" frameborder="0" style="border:none; overflow:hidden; width:250px; height:400px; background: white; " allowTransparency="true">
+					</iframe>
 				</div>
 			</div>
-			<div class="col-md-3">
-				<div class="pages ml-4">
-					<h3 class="mt-4">Pages</h3>
-					<ul class="list-unstyled mt-4">
-						<li><a href="#">home</a> </li>
-						<li><a href="#">current opening</a> </li>
-						<li><a href="#">Walk in</a> </li>
-						<li><a href="#">faq</a> </li>
-					</ul>
-				</div>
-			</div>
-			<div class="col-md-3">
-				<div class="contact">
-					<h3 class="mt-4">Address</h3>
-					<p class="mt-4">Address: 3rd Floor, Building No. 261, Lane No 5, Westend Marg, Said-ul-Ajaib, Saket</p>
-					<p>Phone : 12554854555</p>
-				</div>
-			</div>
-			<div class="col-md-3">
-				<iframe  src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2FMercifulServantHD&width=250&colorscheme=light&show_faces=true&border_color&stream=true&header=true&height=500"
-				scrolling="yes" frameborder="0" style="border:none; overflow:hidden; width:250px; height:500px; background: white; " allowTransparency="true">
-				</iframe>
-			</div>
+		</div>
+		<div class="footer-strip">
+			© Designed and developed by Wajahatsyed | All rights reserved for Al-Ashjaar pvt. ltd.
 		</div>
 	</div>
 </section>
 <!-- footer end -->
+</body>
+</html>
+
 	<!-- bootstrap js -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
